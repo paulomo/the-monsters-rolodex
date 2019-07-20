1013806ec1de0294b7fe0a217902d241f1845362
Paul <admin@petbulb.com>
2019-07-19 22:20:21 -0400

    Updates

---
+++ precache-manifest.a20aa278da745a645ba4e526f85e2008.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83d9e82e19cbccb7c0be010430f6c77a",
+    "url": "/the-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a2acc216f2cd79737ef6",
+    "url": "/the-monsters-rolodex/static/css/main.86bdb224.chunk.css"
+  },
+  {
+    "revision": "6ea22432601858ce9ef5",
+    "url": "/the-monsters-rolodex/static/js/2.27e9ad4f.chunk.js"
+  },
+  {
+    "revision": "a2acc216f2cd79737ef6",
+    "url": "/the-monsters-rolodex/static/js/main.977a2f4b.chunk.js"
+  },
+  {
+    "revision": "5b41479fdd7fd3309677",
+    "url": "/the-monsters-rolodex/static/js/runtime~main.dcf9a19d.js"
+  }
+]);
